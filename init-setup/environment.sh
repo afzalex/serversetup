a1168b68653556a3ee8a6a6d31ef058db0a8a6a5
Mohammad Afzal <mafzal@calibo.com>
2022-09-23 00:16:23 +0530

    Updated install-git.sh

--- init-setup/environment.sh
+++ init-setup/environment.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 
+export FZ_EMAIL=afzalex.store@gmail.com
+export "FZ_NAME=Mohammad Afzal"
--- init-setup/install-git.sh
+++ init-setup/install-git.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+sudo apt-get install -y git
+
+source environment.sh
+git config --global user.email "${FZ_EMAIL}"
+git config --global user.name "${FZ_NAME}"
