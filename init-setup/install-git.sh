0f377dd3ac6be72c4c40d19f40ea8ca6812743de
Mohammad Afzal <mafzal@calibo.com>
2022-09-23 00:24:51 +0530

    Updated install-git.sh

--- init-setup/install-git.sh
+++ init-setup/install-git.sh
@@ -5,5 +5,8 @@ sudo apt-get install -y git
 source environment.sh
 git config --global user.email "${FZ_EMAIL}"
 git config --global user.name "${FZ_NAME}"
+git config --global pull.rebase false
 
 git remote set-url origin git@github.com:afzalex/serversetup.git
+
+curl https://www.afzalex.com/scripts/install-flog.sh | /bin/bash
