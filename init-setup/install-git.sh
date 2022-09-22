4d786df648811425f864f1b3ed61465995cb3c3f
Mohammad Afzal <mafzal@calibo.com>
2022-09-23 00:18:36 +0530

    Updated install-git.sh

--- init-setup/install-git.sh
+++ init-setup/install-git.sh
@@ -5,3 +5,5 @@ sudo apt-get install -y git
 source environment.sh
 git config --global user.email "${FZ_EMAIL}"
 git config --global user.name "${FZ_NAME}"
+
+git remote set-url origin git@github.com:afzalex/serversetup.git
