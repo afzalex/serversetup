f3a5f5b0576ef42c4fc0754b9e206ebb2c82cb9c
Mohammad Afzal <afzalex.store@gmail.com>
2024-07-14 19:25:34 +0530

    traefik restructuring

--- traefik/setup.sh
+++ traefik/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+rm -rf .build
 mkdir -p .build/dynamic
 
 traefik_url='https://github.com/traefik/traefik/releases/download/v3.0.4/traefik_v3.0.4_linux_arm64.tar.gz'
@@ -13,8 +14,8 @@ tar -xzvf "$filename" -C .build "traefik"
 export TRAEFIK_LOC="$PWD/.build"
 export TRAEFIK_SSL_LOC="$PWD/ssl"
 
-envsubst '$TRAEFIK_LOC $TRAEFIK_SSL_LOC' < traefik.toml.tpl > .build/traefik.toml
-cp *.toml .build/dynamic/
+envsubst '$TRAEFIK_LOC $TRAEFIK_SSL_LOC' < traefik.yaml.tpl > .build/traefik.yaml
+cp *.yaml .build/dynamic/
 envsubst '$TRAEFIK_LOC' < traefik.service.tpl > .build/traefik.service
 
 sudo cp .build/traefik.service /etc/systemd/system/traefik.service
